44c510fe9041a869a717620d0398d9b048cdcc23
Juan Pablo Chancay <102939810+jpcpol@users.noreply.github.com>
2023-12-06 15:32:51 -0300

    Add files via upload

---
+++ pykeepapp.py
@@ -0,0 +1,107 @@
+import pykeepass
+from pysnmp.hlapi import *
+import ping3
+import time
+
+telegram_bot_api = "6008447382:AAG-tSYm3NXbiMGlk6KXrRpr1AtN9PWLIIA"
+
+def teleSMS(sms):
+    import requests
+
+    TOKEN = "5877184377:AAGtpkkn1vv8En9dOAI8ud1Z-mE6AdUNk34"
+    #CHAT_ID = "448460485"
+    #CHAT_ID2 = "5877184377"
+    GROUP_ID  = "-1001973955375"
+
+    send_text = 'https://api.telegram.org/bot' + TOKEN + '/sendMessage?chat_id=' + GROUP_ID + '&parse_mode=Markdown&text=' + sms
+
+    try:
+        response = requests.get(send_text)
+    except:
+        teleSMS('Error teleSMS')
+    
+    
+def alive(address):
+    lost = 0
+    for cont in range(3):
+        if ping3.ping(address) == None:
+            lost += 1    
+        
+
+    if lost >= 2:
+        
+        return False
+    
+    else:
+        teleSMS('Error Ping')
+        return True
+
+def getOID(address):
+    oid = "1.3.6.1.4.1.1918.2.13.10.40.10.0"
+    comunidad = "public"
+    puerto = 161
+    
+    resultado = next(getCmd(SnmpEngine(),
+                    CommunityData(comunidad),
+                    UdpTransportTarget((address, puerto)),
+                    ContextData(),
+                    ObjectType(ObjectIdentity(oid))))
+
+    try:
+        return str(resultado[3][0].prettyPrint()[-3:])
+    except:
+        teleSMS('Error getOID')
+        return False
+
+
+print("Comienza el Monitoreo")   
+alarmas = {} #inicializamos el diccionario
+alcanzables = {} # inicializamos diccionario de alcanzabilidad
+
+# abre la base de datos
+
+db = pykeepass.PyKeePass(r'\\intranet.sanjuan.edu.ar\DavWWWRoot\rea de Sistemas\Infraestructura IT\Red Interescolar\password.kdbx', password='aITX8QekV9IwS3f8Xij8')
+
+
+
+while True:
+    # itera a través de cada grupo
+    for group in db.groups:
+        #print("Grupo: " + group.name)
+    
+        if group.name == "Traza Valle Fértil" or group.name == "Traza Jáchal" or group.name == "Traza Tontal":
+        
+            # itera a través de cada entrada en el grupo
+            for entry in group.entries:
+                
+                print(entry.url[8:])
+            
+                if entry.title != None and entry.url != None and alive(entry.url[8:]) != False and getOID(entry.url[8:]) != False:
+                    
+                    #print('Primer If')
+                    
+                    if entry.url[8:] in alarmas:
+                    
+                        del alarmas[entry.url[8:]]
+                        
+                        teleSMS(entry.title + "\t" + "El dispositivo normalizó el voltaje de entrada")
+                
+                    elif entry.url[8:] in alcanzables:
+                    
+                        del alcanzables[entry.url[8:]]
+                        
+                        teleSMS(entry.title + "\t" + "El dispositivo volvió a tener conectividad")
+            
+                elif entry.title != None and entry.url != None and alive(entry.url[8:]) != False and getOID(entry.url[8:]) == False and entry.url[8:] not in alarmas:      
+                                                      
+                    alarmas[entry.url[8:]] = entry.title
+                
+                    teleSMS(entry.title + "\t" + "Voltaje de Entrada: " + getOID(entry.url[8:]))      
+                           
+                elif entry.title != None and entry.url != None and alive(entry.url[8:]) != True and entry.url[8:] not in alcanzables:
+                    
+                    alcanzables[entry.url[8:]] = entry.title
+                
+                    teleSMS(entry.title + "\t" + "Dispositivo Inalcanzable ")
+                              
+    time.sleep(300)
