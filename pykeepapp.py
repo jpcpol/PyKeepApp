cc52e0f355721613ece231cedfdb128cc9d4819c
jpcpol <jpcpol@gmail.com>
2023-12-14 13:30:42 -0300

    Revert

--- pykeepapp.py
+++ pykeepapp.py
@@ -60,11 +60,8 @@ def getOID(address):
 print("Comienza el Monitoreo")   
 alarmas = {} #inicializamos el diccionario
 alcanzables = {} # inicializamos diccionario de alcanzabilidad
-db = []
-try:
-    db = pykeepass.PyKeePass('H:\Unidades compartidas\Operaciones\Mantenimiento\password.kdbx', password='aITX8QekV9IwS3f8Xij8')  # abre la base de datos
-except:
-    print('Error de DB')
+
+db = pykeepass.PyKeePass(r'\\intranet.sanjuan.edu.ar\DavWWWRoot\rea de Sistemas\Infraestructura IT\Red Interescolar\password.kdbx', password='aITX8QekV9IwS3f8Xij8')  # abre la base de datos
 
 while True:
     # itera a través de cada grupo
